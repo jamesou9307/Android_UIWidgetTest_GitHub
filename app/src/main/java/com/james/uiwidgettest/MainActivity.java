f5178a4a2167d0358061bbf68539a9e54393a2f8
JAMES <291387044@qq.com>
2017-08-07 22:42:54 +0800

    initial commit

--- app/src/main/java/com/james/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/james/uiwidgettest/MainActivity.java
@@ -2,6 +2,9 @@ package com.james.uiwidgettest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity_layout);
+        Button button1=(Button)findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newText="fxxk you";
+                TextView textView1=(TextView)findViewById(R.id.text_view);
+                textView1.setText(newText);
+            }
+        });
     }
 }
