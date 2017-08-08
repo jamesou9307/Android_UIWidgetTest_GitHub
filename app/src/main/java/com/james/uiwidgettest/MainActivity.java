6dc99fa1b2026d686ae0b5e19a5b0fbb5371c27e
jamesou9307 <291387044@qq.com>
2017-08-08 15:38:38 +0800

    new commit

--- app/src/main/java/com/james/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/james/uiwidgettest/MainActivity.java
@@ -4,7 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +20,34 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
+                /* //更改textview的显示内容
                 String newText="fxxk you";
                 TextView textView1=(TextView)findViewById(R.id.text_view);
-                textView1.setText(newText);
+                textView1.setText(newText);*/
+
+                /*
+                * //获取edittext的输入内容
+                EditText editText1=(EditText)findViewById(R.id.edit_test);
+                Toast.makeText(MainActivity.this,editText1.getText(),Toast.LENGTH_SHORT).show();
+                 */
+
+                /*
+                //更改显示的图片
+                ImageView imageView=(ImageView)findViewById(R.id.img_view);
+                imageView.setImageResource(R.drawable.img2);
+                */
+                ProgressBar progressBar=(ProgressBar)findViewById(R.id.progress_bar);
+                /*if(progressBar.getVisibility()==View.GONE){
+                    progressBar.setVisibility(View.VISIBLE);
+
+                }
+                else {
+                    progressBar.setVisibility(View.GONE);
+                }*/
+                int progress=progressBar.getProgress();
+                progressBar.setProgress(progress+10);
             }
         });
     }
